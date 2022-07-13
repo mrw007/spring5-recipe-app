af1442143f1254d9aac03ca6394d4ade9067bd38
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-13 14:01:23 +0100

    Added Category Entity and Many-to-Many relationship

--- src/main/java/mrw007/springframework/spring5recipeapp/models/Recipe.java
+++ src/main/java/mrw007/springframework/spring5recipeapp/models/Recipe.java
@@ -26,6 +26,9 @@ public class Recipe {
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -121,5 +124,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }

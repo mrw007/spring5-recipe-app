9c5b2034dea5b3e2f3eedaace8bc864814c57bcb
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-18 14:30:42 +0100

    Added Service Unit Test Example

---
+++ src/test/java/mrw007/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package mrw007.springframework.spring5recipeapp.services;
+
+import mrw007.springframework.spring5recipeapp.models.Recipe;
+import mrw007.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}

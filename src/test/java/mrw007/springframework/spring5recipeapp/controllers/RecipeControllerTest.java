6de2540394c82f2ac64adab7e63ea700ee3b8026
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-24 13:26:19 +0100

    Updated show recipe page attributes to be dynamic

--- src/test/java/mrw007/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/mrw007/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -2,23 +2,19 @@ package mrw007.springframework.spring5recipeapp.controllers;
 
 import mrw007.springframework.spring5recipeapp.models.Recipe;
 import mrw007.springframework.spring5recipeapp.services.RecipeService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class RecipeControllerTest {
@@ -30,7 +26,7 @@ class RecipeControllerTest {
     RecipeController controller;
 
     @BeforeEach()
-    public void setUp() throws Exception {
+    public void setUp() {
         controller = new RecipeController(recipeService);
     }
 
@@ -44,8 +40,8 @@ class RecipeControllerTest {
 
         when(recipeService.findById(anyLong())).thenReturn(recipe);
 
-        mockMvc.perform(get("/recipe/show/1"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+        mockMvc.perform(get("/recipe/show/1")).andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 }

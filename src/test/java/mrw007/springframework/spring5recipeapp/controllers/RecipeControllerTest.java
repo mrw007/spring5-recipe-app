c75f7d3b7d7286642b760684a2750654e0c8c9c0
Wahib Kerkeni <mr.wahib@gmail.com>
2022-07-24 12:31:59 +0100

    Added Recipe Controller

---
+++ src/test/java/mrw007/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,51 @@
+package mrw007.springframework.spring5recipeapp.controllers;
+
+import mrw007.springframework.spring5recipeapp.models.Recipe;
+import mrw007.springframework.spring5recipeapp.services.RecipeService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    RecipeController controller;
+
+    @BeforeEach()
+    public void setUp() throws Exception {
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}

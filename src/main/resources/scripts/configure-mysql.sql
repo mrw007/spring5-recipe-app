03c09085d0a02e80670123da0324ca2ab75c9b6c
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-11 14:56:44 +0100

    Added Spring Boot Configuration for MySQL

--- src/main/resources/scripts/configure-mysql.sql
+++ src/main/resources/scripts/configure-mysql.sql
@@ -3,8 +3,8 @@
 
 # connect to mysql and run as root user
 #Create Databases
-CREATE DATABASE mrw_dev;
-CREATE DATABASE mrw_prod;
+CREATE DATABASE recipe_app_db_dev;
+CREATE DATABASE recipe_app_db_prod;
 
 #Create database service accounts
 CREATE USER 'mrw_dev_user'@'localhost' IDENTIFIED BY 'pass';
@@ -13,19 +13,19 @@ CREATE USER 'mrw_dev_user'@'%' IDENTIFIED BY 'pass';
 CREATE USER 'mrw_prod_user'@'%' IDENTIFIED BY 'pass';
 
 #Database grants
-GRANT SELECT ON mrw_dev.* to 'mrw_dev_user'@'localhost';
-GRANT INSERT ON mrw_dev.* to 'mrw_dev_user'@'localhost';
-GRANT DELETE ON mrw_dev.* to 'mrw_dev_user'@'localhost';
-GRANT UPDATE ON mrw_dev.* to 'mrw_dev_user'@'localhost';
-GRANT SELECT ON mrw_prod.* to 'mrw_prod_user'@'localhost';
-GRANT INSERT ON mrw_prod.* to 'mrw_prod_user'@'localhost';
-GRANT DELETE ON mrw_prod.* to 'mrw_prod_user'@'localhost';
-GRANT UPDATE ON mrw_prod.* to 'mrw_prod_user'@'localhost';
-GRANT SELECT ON mrw_dev.* to 'mrw_dev_user'@'%';
-GRANT INSERT ON mrw_dev.* to 'mrw_dev_user'@'%';
-GRANT DELETE ON mrw_dev.* to 'mrw_dev_user'@'%';
-GRANT UPDATE ON mrw_dev.* to 'mrw_dev_user'@'%';
-GRANT SELECT ON mrw_prod.* to 'mrw_prod_user'@'%';
-GRANT INSERT ON mrw_prod.* to 'mrw_prod_user'@'%';
-GRANT DELETE ON mrw_prod.* to 'mrw_prod_user'@'%';
-GRANT UPDATE ON mrw_prod.* to 'mrw_prod_user'@'%';
+GRANT SELECT ON recipe_app_db_dev.* to 'mrw_dev_user'@'localhost';
+GRANT INSERT ON recipe_app_db_dev.* to 'mrw_dev_user'@'localhost';
+GRANT DELETE ON recipe_app_db_dev.* to 'mrw_dev_user'@'localhost';
+GRANT UPDATE ON recipe_app_db_dev.* to 'mrw_dev_user'@'localhost';
+GRANT SELECT ON recipe_app_db_prod.* to 'mrw_prod_user'@'localhost';
+GRANT INSERT ON recipe_app_db_prod.* to 'mrw_prod_user'@'localhost';
+GRANT DELETE ON recipe_app_db_prod.* to 'mrw_prod_user'@'localhost';
+GRANT UPDATE ON recipe_app_db_prod.* to 'mrw_prod_user'@'localhost';
+GRANT SELECT ON recipe_app_db_dev.* to 'mrw_dev_user'@'%';
+GRANT INSERT ON recipe_app_db_dev.* to 'mrw_dev_user'@'%';
+GRANT DELETE ON recipe_app_db_dev.* to 'mrw_dev_user'@'%';
+GRANT UPDATE ON recipe_app_db_dev.* to 'mrw_dev_user'@'%';
+GRANT SELECT ON recipe_app_db_prod.* to 'mrw_prod_user'@'%';
+GRANT INSERT ON recipe_app_db_prod.* to 'mrw_prod_user'@'%';
+GRANT DELETE ON recipe_app_db_prod.* to 'mrw_prod_user'@'%';
+GRANT UPDATE ON recipe_app_db_prod.* to 'mrw_prod_user'@'%';
